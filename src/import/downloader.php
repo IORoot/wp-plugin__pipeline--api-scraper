d2a035866b454bb706b52fe8297a613e1c86633c
Andy Pearson <andy@londonparkour.com>
2020-07-04 10:02:25 +0100

    Merge local and remote method

--- src/import/downloader.php
+++ src/import/downloader.php
@@ -32,22 +32,14 @@ class downloader
             return $image_id;  
         }
 
-        // Check if this is a LOCAL file here. (regex for leading slash on url / )
-        if (preg_match('/^\//', $url) == 1)
+        $this->tmp = $this->url;
+
+        // Check if this is a REMOTE file. (regex for leading 'HTTP'' )
+        if (preg_match('/^http/i', $url) == 1)
         {
-            return $this->download_local();
+            $this->download_url();
         }
 
-        // download a remote file.
-        return $this->download_remote($url, $post_data, $alttext, $filename );
-        
-    }
-
-
-    public function download_local()
-    {
-        //$tmp      = $this->download_url();
-        $this->tmp        = $this->url;
         $this->check_filetype();
         $this->rename_output_filename();
         $this->build_file_array_for_sideload();
@@ -58,6 +50,7 @@ class downloader
 
         return $this->att_id;
     }
+
 
 
     public function download_url()
@@ -169,88 +162,6 @@ class downloader
 
         return;
     }
-
-
-
-
-
-
-    public function download_remote($url = null, $post_data = array(), $alttext, $filename = null )
-    {
-        // Download the file to temporary storage
-        require_once( ABSPATH . 'wp-admin/includes/file.php' );
-        $tmp = download_url( $url );
-
-        // If error storing temporarily, unlink
-        if ( is_wp_error( $tmp ) ) {
-            @unlink($file_array['tmp_name']);   // clean up
-            $file_array['tmp_name'] = '';
-            return $tmp; // output wp_error
-        }
-
-        preg_match('/[^\?]+\.(jpg|JPG|jpe|JPE|jpeg|JPEG|gif|GIF|png|PNG)/', $url, $matches);    // fix file filename for query strings
-        $url_filename = basename($matches[0]);                                                  // extract filename from url for title
-        $url_type = wp_check_filetype($url_filename);                                           // determine file type (ext and mime/type)
-
-        // override filename if given, reconstruct server path
-        if ( !empty( $filename ) ) {
-            $filename = sanitize_file_name($filename);
-            $tmppath = pathinfo( $tmp );                                                        // extract path parts
-            $new = $tmppath['dirname'] . "/". $filename . "." . $tmppath['extension'];          // build new path
-            rename($tmp, $new);                                                                 // renames temp file on server
-            $tmp = $new;                                                                        // push new filename (in path) to be used in file array later
-        }
-
-        // assemble file data (should be built like $_FILES since wp_handle_sideload() will be using)
-        // $filename: array(2)
-        //      tmp_name: "/tmp/filename.tmp"
-        //      name: "newname.jpg"
-        $file_array['tmp_name'] = $tmp;                                                         // full server path to temp file
-        if ( !empty( $filename ) ) {
-            $file_array['name'] = $filename . "." . $url_type['ext'];                           // user given filename for title, add original URL extension
-        } else {
-            $file_array['name'] = $url_filename;                                                // just use original URL filename
-        }
-
-        // set additional wp_posts columns
-        if ( empty( $post_data['post_title'] ) ) {
-            $post_data['post_title'] = basename($url_filename, "." . $url_type['ext']);         // just use the original filename (no extension)
-        }
-        // To distinguish between a post and it's image,
-        // add on the word 'image' to the title at the end.
-        // otherwise when we're checking for an existing
-        // post, it'll see the image as a post with the same
-        // title and say it exists!
-        // By adding the word 'title' it'll be different
-        // to the post title slightly.
-        $post_data['post_title'] = $post_data['post_title'] . ' image';
-
-
-        // required libraries for media_handle_sideload
-        require_once(ABSPATH . 'wp-admin/includes/file.php');
-        require_once(ABSPATH . 'wp-admin/includes/media.php');
-        require_once(ABSPATH . 'wp-admin/includes/image.php');
-
-
-        // do the validation and storage stuff
-        // $post_data can override the items saved to wp_posts table, like post_mime_type, guid, post_parent, post_title, post_content, post_status
-        //
-        // Note - $post_id is NULL. This is because we'll attach the image to the post with the
-        // 'attach' class instead.
-        $att_id = media_handle_sideload( $file_array, null, null, $post_data );             
-
-        // Set the image Alt-Text
-        update_post_meta( $att_id, '_wp_attachment_image_alt', $alttext );
-
-        // If error storing permanently, unlink
-        if ( is_wp_error($att_id) ) {
-            @unlink($file_array['tmp_name']);   // clean up
-            return $att_id; // output wp_error
-        }
-
-        return $att_id;
-    }
-
 
 
     public function does_image_exist($filename)
