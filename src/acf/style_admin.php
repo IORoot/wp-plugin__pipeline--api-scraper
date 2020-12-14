6059992d222f0ba3422fed4e98c487d261e87e87
Andy Pearson <andy@londonparkour.com>
2020-12-14 11:31:13 +0000

    Updated icons and layout

--- src/acf/style_admin.php
+++ src/acf/style_admin.php
@@ -5,6 +5,7 @@ function my_acf_admin_head() {
     <style type="text/css">
         <?php include 'css/admin.css'; ?>
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@5.8.55/css/materialdesignicons.min.css" rel="stylesheet">
     <?php
 }
 
